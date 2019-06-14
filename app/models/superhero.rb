b6922575f1a71e40ffbfecdef0b9aead1a43d105
Tony Baker <this.tonybaker@gmail.com>
2019-06-14 21:25:40 +0000

    Done.

---
+++ app/models/superhero.rb
@@ -0,0 +1,16 @@
+class Superhero
+  attr_accessor :name, :power, :bio
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
